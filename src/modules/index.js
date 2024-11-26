5cb852d4cafe456ef51b7146af3cc502ef4624a0
naomi-ho <151114635+naomi-ho@users.noreply.github.com>
2024-11-26 16:08:11 -0500

    Add function where dropdown disappears when user clicks out of dropdown content

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,5 +1,17 @@
 import "../style.css"
 
-window.toggleDropdown = function () {
+window.toggleDropdown = () =>
   document.querySelector(".dropdown-content").classList.toggle("show")
+
+// when user clicks out of dropdown content, list disappears
+window.onclick = (e) => {
+  if (!e.target.matches(".dropdownbtn")) {
+    const dropdowns = document.getElementsByClassName("dropdown-content")
+    for (let i = 0; i < dropdowns.length; i++) {
+      let openDropdown = dropdowns[i]
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show")
+      }
+    }
+  }
 }
